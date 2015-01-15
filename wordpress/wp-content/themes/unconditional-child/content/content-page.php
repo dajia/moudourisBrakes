e4baaa4be269aa77cfae236714a2b2bff73e0a97
dajia <dajia.dim@gmail.com>
2015-01-15 16:16:49 +0200

    inline style for h1

--- wordpress/wp-content/themes/unconditional-child/content/content-page.php
+++ wordpress/wp-content/themes/unconditional-child/content/content-page.php
@@ -9,7 +9,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
 	<header class="page-header">
-		<h1 class="page-title"><?php the_title(); ?></h1>
+		<h1 class="page-title" style="display:none;"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
 	
 	<div class="entry-content">
