f2ada29cb031634c2ec3b7884de5c5f9e3101aea
dajia <dajia.dim@gmail.com>
2015-03-31 09:12:37 +0300

    Add col-md-12 to article header

--- wordpress/wp-content/themes/unconditional-child/content.php
+++ wordpress/wp-content/themes/unconditional-child/content.php
@@ -5,7 +5,7 @@
 ?>
 <div class="col-md-3 col-sm-4 col-sm-12">
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	<header class="entry-header col-sm-12">
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
