4940f78b4e562dcebf6ce0c71144f1c35b7e74e9
dajia <dajia.dim@gmail.com>
2015-03-31 09:22:54 +0300

    remove comments from map template

--- wordpress/wp-content/themes/unconditional-child/templates/map.php
+++ wordpress/wp-content/themes/unconditional-child/templates/map.php
@@ -11,10 +11,6 @@ get_header(); ?>
 <section class="container-fluid" id="section3">
   <div class="row">
     <div class="col-sm-12">
-        <?php //if ( is_active_sidebar( 'page' ) ) : ?>
-        <!--<div class="col-sm-12">-->
-          <?php //else : ?>
-            <?php //endif; ?>
             <?php do_action( 'unconditional_before_page' ); ?>
             <?php while ( have_posts() ) : the_post(); ?>
 
@@ -28,18 +24,8 @@ get_header(); ?>
 
             <?php endwhile; // end of the loop. ?>
             <?php do_action( 'unconditional_after_page' ); ?>
-       <!-- <div class="col-xs-12 col-sm-12 col-md-12">
-          <div class="img-map">
-            <img src="<?php //echo get_stylesheet_directory_uri(); ?>/assets/images/map2.jpg">
-          </div>
-        </div>-->
           <?php if ( !is_active_sidebar( 'page' ) ) { ?>
-
-
-
-		  <!--</div>-->
           <?php } ?>
-
     </div>
   </div>
 </section>
