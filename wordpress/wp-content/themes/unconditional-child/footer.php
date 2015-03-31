93603f1bb62b22d4bcefdbc0e45269743fbba251
dajia <dajia.dim@gmail.com>
2015-03-31 09:34:03 +0300

    Clean up comments

--- wordpress/wp-content/themes/unconditional-child/footer.php
+++ wordpress/wp-content/themes/unconditional-child/footer.php
@@ -11,12 +11,9 @@
         <a href="mailto:dajia@polyptychon.gr"><i class="fa fa-envelope fa-lg"></i></a>
         <!--<a href="http://www.googlemaps.com" target="_blank"><i class="fa fa-map-marker fa-lg"></i></a></span>-->
         <a href="/epikoinonia-frena-moudouris/map/"><i class="fa fa-map-marker fa-lg"></i></a></span>
-      <!--
-			<?php do_action( 'unconditional_before_credits' );
-			//printf( __( 'Proudly Powered By', 'unconditional' ) ); ?><a target="_Blank" href="<?php //echo esc_url( __( 'http://wordpress.org/', 'unconditional' ) ); ?>" title="<?php //esc_attr_e( 'Semantic Personal Publishing Platform', 'unconditional' ); ?>"><?php //printf( __( ' %s', 'unconditional' ), 'WordPress' ); ?></a>
-			<!--<span class="sep"> | </span>
-			<?php //printf( __( 'Theme', 'unconditional' ) ); ?><a target="_blank" href="<?php //echo esc_url( __( 'http://www.wpstrapcode.com/blog/unconditional', 'unconditional' ) ); ?>" title="<?php //esc_attr_e( 'Unconditional WordPress Theme', 'unconditional' ); ?>"><?php //printf( __( ' %s', 'unconditional' ), 'Unconditional' ); ?></a><?php //printf( __( ' By %s', 'unconditional' ), 'WP Strap Code' );
-		do_action( 'unconditional_after_credits' ); ?>
+
+			<?php do_action( 'unconditional_before_credits' ); ?>
+		<?php do_action( 'unconditional_after_credits' ); ?>
 	    -->
       </div>
 	    </div><!-- .site-info -->
