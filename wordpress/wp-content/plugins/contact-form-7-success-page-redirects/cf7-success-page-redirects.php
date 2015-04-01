41b53be43bc525036068c84bd11eadf0951bdfd8
dajia <dajia.dim@gmail.com>
2015-04-01 14:53:32 +0300

    Add contact form 7 success page redirect plugin

---
+++ wordpress/wp-content/plugins/contact-form-7-success-page-redirects/cf7-success-page-redirects.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Plugin Name: Contact Form 7 - Success Page Redirects
+ * Description: An add-on for Contact Form 7 that provides a straightforward method to redirect visitors to success pages or thank you pages.
+ * Version: 1.1.6
+ * Author: Ryan Nevius
+ * Author URI: http://www.ryannevius.com
+ * License: GPLv3
+ */
+
+/**
+ * Verify CF7 dependencies.
+ */
+function cf7_success_page_admin_notice() {
+    // Verify that CF7 is active and updated to the required version (currently 3.9.0)
+    if ( is_plugin_active('contact-form-7/wp-contact-form-7.php') ) {
+        $wpcf7_path = plugin_dir_path( dirname(__FILE__) ) . 'contact-form-7/wp-contact-form-7.php';
+        $wpcf7_plugin_data = get_plugin_data( $wpcf7_path, false, false);
+        $wpcf7_version = (int)preg_replace('/[.]/', '', $wpcf7_plugin_data['Version']);
+        // CF7 drops the ending ".0" for new major releases (e.g. Version 4.0 instead of 4.0.0...which would make the above version "40")
+        // We need to make sure this value has a digit in the 100s place.
+        if ( $wpcf7_version < 100 ) {
+            $wpcf7_version = $wpcf7_version * 10;
+        }
+        // If CF7 version is < 3.9.0
+        if ( $wpcf7_version < 390 ) {
+            echo '<div class="error"><p><strong>Warning: </strong>Contact Form 7 - Success Page Redirects requires that you have the latest version of Contact Form 7 installed. Please upgrade now.</p></div>';
+        }
+    }
+    // If it's not installed and activated, throw an error
+    else {
+        echo '<div class="error"><p>Contact Form 7 is not activated. The Contact Form 7 Plugin must be installed and activated before you can use Success Page Redirects.</p></div>';
+    }
+}
+add_action( 'admin_notices', 'cf7_success_page_admin_notice' );
+
+
+/**
+ * Disable Contact Form 7 JavaScript completely
+ */
+add_filter( 'wpcf7_load_js', '__return_false' );
+
+
+/**
+ * Adds a box to the main column on the form edit page.
+ */
+// Register the meta boxes
+function cf7_success_page_settings() {
+    add_meta_box( 'cf7-redirect-settings', 'Success Page Redirect', 'cf7_success_page_metaboxes', '', 'form', 'low');
+}
+add_action( 'wpcf7_add_meta_boxes', 'cf7_success_page_settings' );
+
+// Create the meta boxes
+function cf7_success_page_metaboxes( $post ) {
+    wp_nonce_field( 'cf7_success_page_metaboxes', 'cf7_success_page_metaboxes_nonce' );
+    $cf7_success_page = get_post_meta( $post->id(), '_cf7_success_page_key', true );
+
+    // The meta box content
+    echo '<label for="cf7-redirect-page-id"><strong>Redirect to: </strong></label><br> ';
+    $dropdown_options = array (
+            'name' => 'cf7-redirect-page-id', 
+            'show_option_none' => '--', 
+            'option_none_value' => '0',
+            'selected' => $cf7_success_page
+        );
+    wp_dropdown_pages( $dropdown_options );
+}
+
+// Store Success Page Info
+function cf7_success_page_save_contact_form( $contact_form ) {
+    $contact_form_id = $contact_form->id();
+
+    if ( !isset( $_POST ) || empty( $_POST ) || !isset( $_POST['cf7-redirect-page-id'] ) ) {
+        return;
+    }
+    else {
+        // Verify that the nonce is valid.
+        if ( ! wp_verify_nonce( $_POST['cf7_success_page_metaboxes_nonce'], 'cf7_success_page_metaboxes' ) ) {
+            return;
+        }
+        // Update the stored value
+        update_post_meta( $contact_form_id, '_cf7_success_page_key', $_POST['cf7-redirect-page-id'] );
+    }
+}
+add_action( 'wpcf7_after_save', 'cf7_success_page_save_contact_form' );
+
+
+/**
+ * Copy Redirect page key and assign it to duplicate form
+ */
+function cf7_success_page_after_form_create( $contact_form ){
+    $contact_form_id = $contact_form->id();
+
+    // Get the old form ID
+    if ( !empty( $_REQUEST['post'] ) && !empty( $_REQUEST['_wpnonce'] ) ) {
+        $old_form_id = get_post_meta( $_REQUEST['post'], '_cf7_success_page_key', true );
+    }
+    // Update the duplicated form
+    update_post_meta( $contact_form_id, '_cf7_success_page_key', $old_form_id );
+}
+add_action( 'wpcf7_after_create', 'cf7_success_page_after_form_create' );
+
+
+/**
+ * Redirect the user, after a successful email is sent
+ */
+function cf7_success_page_form_submitted( $contact_form ) {
+    $contact_form_id = $contact_form->id();
+
+    // Send us to a success page, if there is one
+    $success_page = get_post_meta( $contact_form_id, '_cf7_success_page_key', true );
+    if ( !empty($success_page) ) {
+        wp_redirect( get_permalink( $success_page ) );
+        die();
+    }
+}
+add_action( 'wpcf7_mail_sent', 'cf7_success_page_form_submitted' );
---
+++ wordpress/wp-content/plugins/contact-form-7-success-page-redirects/readme.txt
@@ -0,0 +1,77 @@
+=== Contact Form 7 - Success Page Redirects ===
+Contributors: rnevius
+Tags: contact form 7, cf7, contact forms 7, contact form, redirect, forms, form redirect, form, success pages, thank you pages, contact form 7 add-on, cf7 redirect, cf7 success, contact form 7 redirect, contact form 7 success
+Requires at least: 3.8.2
+Tested up to: 4.1
+Stable tag: trunk
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+An add-on for Contact Form 7 that provides a straightforward method to redirect visitors to success pages or thank you pages.
+
+
+== Description ==
+
+An add-on for Contact Form 7 (CF7) that provides a straightforward method to redirect visitors to success pages or thank you pages, if their messages are successfully delivered. If no message is sent, or if there is an error with the form, the user will not be redirected.
+
+**NOTE:** This plugin requires Contact Form 7 version 3.9 or later. 
+
+
+== Installation ==
+
+1. Unzip the downloaded plugin archive.
+2. Upload the inner 'contact-form-7-success-page-redirects' directory to the '/wp-content/plugins/' directory on your web server.
+3. Activate the plugin through the 'Plugins' menu in WordPress.
+
+Please note that [Contact Form 7](https://wordpress.org/plugins/contact-form-7/) (version 3.9 or later) must be installed and activated in order for this plugin to be functional.
+
+
+== Frequently Asked Questions ==
+
+= Why doesn't this plugin do *insert feature here*? =
+
+If you have a feature suggestion, I'd love to hear about it. Feel free to leave a message on the "Support" tab of the plugin page, and I'll follow up as soon as possible.
+
+= My forms are no longer submitting, with this plugin installed. Why? =
+
+Please make sure that you have updated to the latest version of Contact Form 7. Success Page Redirects only supports versions 3.9.0 or higher. If everything is up to date and you're still experiencing problems, please post a message on the [Support Page](https://wordpress.org/support/plugin/contact-form-7-success-page-redirects).
+
+= Why are my forms no longer using Ajax, when I install this plugin? =
+
+In order to ensure that all forms are submitted properly, and that users can be redirected to success pages from some of your forms, I have had to disable Contact Form 7's JavaScript for all forms. This means that you won't be able to use Ajax to submit forms. In future updates, I hope to change this functionality, so that Ajax is only disabled for the forms that have a redirect page defined. Please note that disabling CF7's JavaScript does not affect your forms' ability to send messages properly.
+
+
+== Screenshots ==
+
+1. The plugin will add a "Redirect to:" dropdown that contains all of your existing pages as options. This is set on each of the "Edit Contact Form" pages.
+
+
+== Changelog ==
+
+= 1.1.6 =
+* Fixes a bug where duplicating a contact form from the form list view wouldn't copy over the redirect field. 
+
+= 1.1.5 =
+* Fixes a bug where duplicating a contact form wouldn't copy over the redirect field from the original form edit page. 
+
+= 1.1.4 =
+* Upgrade process for verifying that Contact Form 7 is installed and up to date. 
+
+= 1.1.3 =
+* CF7 id property is no longer accessible. Switched to the id() method, so that form changes can be properly saved in the admin.
+
+= 1.1.2 =
+* Provides clearer admin notices regarding necessary plugin dependencies 
+
+= 1.1.1 =
+* Fixes an issue that prevented some forms from submitting and redirecting
+* Disables JavaScript for WPCF7 completely
+
+= 1.1.0 =
+* Initial version
+
+
+== Upgrade Notice ==
+
+= 1.1.1 =
+This version fixes an issue that prevented some forms from submitting. Please upgrade to ensure all CF7 messages are sent.
